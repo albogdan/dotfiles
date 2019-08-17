78a1e3e35fdceb997ed24f97d5f4bf0d3c4172ce
Alex <stefanalexb@gmail.com>
2019-08-17 15:37:51 -0400

    updated i3 install script and added betterlockscreen

---
+++ powerlineinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install powerline fonts-powerline
+
+
+# Then add this to ~/.bashrc
+#if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
+#    source /usr/share/powerline/bindings/bash/powerline.sh
+#fi
+
+
+
+
